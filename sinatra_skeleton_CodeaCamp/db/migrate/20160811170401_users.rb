aa62933830e622774c0d76a120a2e393687d3c0c
codeamac01 <codeamac01@gmail.com>
2016-08-11 20:29:56 -0500

    se añadieron validaciones

--- sinatra_skeleton_CodeaCamp/db/migrate/20160811170401_users.rb
+++ sinatra_skeleton_CodeaCamp/db/migrate/20160811170401_users.rb
@@ -1,12 +1,12 @@
 class Users < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string    :name
-      t.string    :email
+      t.string    :name,  nul: false
+      t.string    :email, nul: false
       #has_secure_password
       #Adds methods to set and authenticate against a BCrypt password.
       #This mechanism requires you to hace a << password_digest >> attribute
-      t.string    :password_digest
+      t.string    :password_digest, nul: false 
 
     end
   end
